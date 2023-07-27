6becc303852c4d48f84abe0ccf601f473a80d013
Yuxuan Liu <97258813+Yu1623@users.noreply.github.com>
2023-07-27 10:45:37 -0700

    Version 4

--- KmerRankCPRepVisual.py
+++ KmerRankCPRepVisual.py
@@ -1,6 +1,6 @@
 '''
-This file analyze kmer rank for the similarity of CP and Rep genes of genomes.
-The program makes a line graph that visualizes the frequency of the similarity percent between CP and Rep genes.
+Purpose: Prompts for a file that has the rank of percents of similar kmers between CP and Rep and makes graphs to visualize the rank.
+The file can be rechieved from CPRepKmerRank.py.
 '''
 
 import numpy as np
@@ -9,6 +9,9 @@ import re
 import sys
 import pandas as pd
 
+'''
+This program converts the file of kmer rank into an associative array that is used for the programs below.
+'''
 def readKmerRanking(fileName):
     f = open(fileName, 'r')
     kmerRank = {}
@@ -18,6 +21,10 @@ def readKmerRanking(fileName):
         percentSimilar = line.split(": ")[1]
         kmerRank[genome] = percentSimilar
     return kmerRank
+
+'''
+This program creates an array of the percents of similar kmers between CP and Rep and an array of the count of each percent.
+'''
 
 def kmerRankShaded(fileName):
     kmerRank = readKmerRanking(fileName)
@@ -41,6 +48,10 @@ def kmerRankShaded(fileName):
         countsSimilar += [percentsCounts[percentSimilar]]
     return percentsSimilar, countsSimilar
 
+'''
+This program creates a line graph with percents of similar kmers between CP and Rep against the counts of the percents.
+'''
+
 def kmerCPRepVisual(fileName):
     percentsSimilar, countsSimilar = kmerRankShaded(fileName)
     plt.plot(percentsSimilar, countsSimilar)
@@ -48,6 +59,10 @@ def kmerCPRepVisual(fileName):
     plt.ylabel("Count")
     plt.title("The Count of percents similar between CP and Rep")
     plt.show()
+
+'''
+This program combines the three line graphs of crucivirus, RNA virus, and DNA virus into one to compare their percents of similarity between CP and Rep.
+'''
 
 def kmerCPRepCompareVisual(Crucifile, RNAfile, DNAfile):
     percentsSimilarCruci, countsSimilarCruci = kmerRankShaded(Crucifile)
