3cf91fc697f4e3c2e8b4ec3acda3ccd04a1630d7
Yuxuan Liu <97258813+Yu1623@users.noreply.github.com>
2023-07-27 11:42:14 -0700

    Version 2

--- checkGenomeSensePrediction.py
+++ checkGenomeSensePrediction.py
@@ -1,6 +1,14 @@
+'''
+Purpose: find the genomes that may show horizontal genetic transfer through unexpected genome sense
+'''
+
 from readFasta import readFasta
 import geneStrandPrediction
 
+'''
+Input: file name and genome name of the gene that the program finds
+Output: the gene that is in the genome
+'''
 def findGene(fileName, genomeName):
     file = readFasta(fileName)
     gene = ""
@@ -9,16 +17,20 @@ def findGene(fileName, genomeName):
             gene = geneName
     return gene
 
-accuracy, incorrectPrediction = geneStrandPrediction.checkPrediction()
-percentsSimilar = 0
-genomeSimilar = []
-for genome in incorrectPrediction:
-    geneNameCP = findGene("879CPs.fasta", genome)
-    geneNameRep = findGene("855Reps.fasta", genome)
-    CPbias, CPpercenta, CPpercentt = geneStrandPrediction.geneStrandPrediction("879CPs.fasta", geneNameCP)
-    Repbias, Reppercenta, Reppercentt = geneStrandPrediction.geneStrandPrediction("855Reps.fasta", geneNameRep)
-    if ((abs(CPpercenta - CPpercentt) > 0.05) or (abs(Reppercenta - Reppercentt) > 0.05)):
-        #print("CP %s percenta: %s, percentt: %s \nRep %s percenta: %s, percentt: %s" % (geneNameCP, CPpercenta, CPpercentt, geneNameRep, Reppercenta, Reppercentt))
-        percentsSimilar += 1
-        genomeSimilar += [genome]
-print(percentsSimilar, genomeSimilar)
+'''
+Identify the genomes that have unexpected genome sense that are different from the prediction
+'''
+def genomeSenseSurprising():
+    accuracy, incorrectPrediction = geneStrandPrediction.checkPrediction()
+    percentsSimilar = 0
+    genomeSimilar = []
+    for genome in incorrectPrediction:
+        geneNameCP = findGene("879CPs.fasta", genome)
+        geneNameRep = findGene("855Reps.fasta", genome)
+        CPbias, CPpercenta, CPpercentt = geneStrandPrediction.geneStrandPrediction("879CPs.fasta", geneNameCP)
+        Repbias, Reppercenta, Reppercentt = geneStrandPrediction.geneStrandPrediction("855Reps.fasta", geneNameRep)
+        if ((abs(CPpercenta - CPpercentt) > 0.05) or (abs(Reppercenta - Reppercentt) > 0.05)):
+            #print("CP %s percenta: %s, percentt: %s \nRep %s percenta: %s, percentt: %s" % (geneNameCP, CPpercenta, CPpercentt, geneNameRep, Reppercenta, Reppercentt))
+            percentsSimilar += 1
+            genomeSimilar += [genome]
+    return percentsSimilar, genomeSimilar
