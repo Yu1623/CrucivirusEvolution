ae110a1d209bc193743fe0b9e50a77f7a7ea1edc
Yuxuan Liu <97258813+Yu1623@users.noreply.github.com>
2023-07-31 14:41:18 -0700

    Version 5

--- KmerRankCPRepVisual.py
+++ KmerRankCPRepVisual.py
@@ -1,17 +1,16 @@
-'''
-Purpose: Prompts for a file that has the rank of percents of similar kmers between CP and Rep and makes graphs to visualize the rank.
-The file can be rechieved from CPRepKmerRank.py.
-'''
-
 import numpy as np
 import matplotlib.pyplot as plt
 import re
 import sys
 import pandas as pd
 
-'''
-This program converts the file of kmer rank into an associative array that is used for the programs below.
-'''
+def main():
+    kmerCPRepVisual("kmerRankCruci.txt")
+    kmerCPRepVisual("kmerRankRNA.txt")
+    kmerCPRepVisual("kmerRankDNA.txt")
+    kmerCPRepCompareVisual("kmerRankCruci.txt", "kmerRankRNA.txt", "kmerRankDNA.txt")
+
+
 def readKmerRanking(fileName):
     f = open(fileName, 'r')
     kmerRank = {}
@@ -21,10 +20,6 @@ def readKmerRanking(fileName):
         percentSimilar = line.split(": ")[1]
         kmerRank[genome] = percentSimilar
     return kmerRank
-
-'''
-This program creates an array of the percents of similar kmers between CP and Rep and an array of the count of each percent.
-'''
 
 def kmerRankShaded(fileName):
     kmerRank = readKmerRanking(fileName)
@@ -48,10 +43,6 @@ def kmerRankShaded(fileName):
         countsSimilar += [percentsCounts[percentSimilar]]
     return percentsSimilar, countsSimilar
 
-'''
-This program creates a line graph with percents of similar kmers between CP and Rep against the counts of the percents.
-'''
-
 def kmerCPRepVisual(fileName):
     percentsSimilar, countsSimilar = kmerRankShaded(fileName)
     plt.plot(percentsSimilar, countsSimilar)
@@ -59,10 +50,6 @@ def kmerCPRepVisual(fileName):
     plt.ylabel("Count")
     plt.title("The Count of percents similar between CP and Rep")
     plt.show()
-
-'''
-This program combines the three line graphs of crucivirus, RNA virus, and DNA virus into one to compare their percents of similarity between CP and Rep.
-'''
 
 def kmerCPRepCompareVisual(Crucifile, RNAfile, DNAfile):
     percentsSimilarCruci, countsSimilarCruci = kmerRankShaded(Crucifile)
@@ -73,6 +60,12 @@ def kmerCPRepCompareVisual(Crucifile, RNAfile, DNAfile):
     plt.plot(percentsSimilarDNA, countsSimilarDNA, alpha = 0.7)
     plt.xlabel("Percent Similar between CP and Rep")
     plt.ylabel("Count")
-    plt.title("Comparison of Count of Similarity Percentage")
+    #plt.title("Comparison of Count of Similarity Percentage")
     plt.legend(["Cruci", "RNA", "DNA"])
     plt.show()
+
+main()
+
+
+
+
