b552f6c68697e31e703fb6e801480ee5e0398a9a
Yuxuan Liu <97258813+Yu1623@users.noreply.github.com>
2023-07-31 12:56:07 -0700

    Version 6

--- checkGenomeSensePrediction.py
+++ checkGenomeSensePrediction.py
@@ -3,7 +3,12 @@ Purpose: find the genomes that may show horizontal genetic transfer through unex
 '''
 
 from readFasta import readFasta
-import geneStrandPrediction
+from geneStrandPrediction import checkPrediction, geneStrandPrediction
+
+def main():
+    percentsSimilar, genomesSimilar = genomeSenseSurprising("875crucisAnnotations.csv", "879CPs.fasta", "855Reps.fasta")
+    print(percentsSimilar)
+    print(genomesSimilar)
 
 '''
 Input: file name and genome name of the gene that the program finds
@@ -22,15 +27,17 @@ Identify the genomes that have unexpected genome sense
 '''
 
 def genomeSenseSurprising(csvfileName, CPfileName, RepfileName):
-    accuracy, incorrectPrediction = geneStrandPrediction.checkPrediction(csvfileName, CPfileName, RepfileName)
+    accuracy, incorrectPrediction = checkPrediction(csvfileName, CPfileName, RepfileName)
     percentsSimilar = 0
     genomeSimilar = []
     for genome in incorrectPrediction:
         geneNameCP = findGene("879CPs.fasta", genome)
         geneNameRep = findGene("855Reps.fasta", genome)
-        CPbias, CPpercenta, CPpercentt = geneStrandPrediction.geneStrandPrediction("879CPs.fasta", geneNameCP)
-        Repbias, Reppercenta, Reppercentt = geneStrandPrediction.geneStrandPrediction("855Reps.fasta", geneNameRep)
+        CPbias, CPpercenta, CPpercentt = geneStrandPrediction("879CPs.fasta", geneNameCP)
+        Repbias, Reppercenta, Reppercentt = geneStrandPrediction("855Reps.fasta", geneNameRep)
         if ((abs(CPpercenta - CPpercentt) > 0.05) and (abs(Reppercenta - Reppercentt) > 0.05)):
             percentsSimilar += 1
             genomeSimilar += [genome]
     return percentsSimilar, genomeSimilar
+
+main()
