53e186d8b07388815dcefea044e9aa8ffbf17e01
Yuxuan Liu <97258813+Yu1623@users.noreply.github.com>
2023-08-04 14:51:25 -0700

    Delete writeFasta.py

--- writeFasta.py
+++
@@ -1,7 +0,0 @@
-import re
-import sys
-def writeFasta(genes, title):
-    file = open("%s.txt" % (title), 'w')
-    for gene in genes:
-        file.write(">" + gene + "\n" + genes[gene] + "\n")
-    file.close()
